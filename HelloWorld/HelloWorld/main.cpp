e3162a826711c78ed240bc8034084a3a1606a8f9
baysibengit <u1097626@umail.utah.edu>
2023-08-21 11:56:17 -0600

    committing my hello world project to git

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Ben Baysinger  on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "first assignmetn of class\n";
+    std::cout << 5+5 << "\n";
+    std::cout << "this semester will be tough\n";
+    return 0;
+}
+    
+
