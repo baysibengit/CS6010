5aa6686edae00e83aa88d6cb9c415632ee463762
baysibengit <u1097626@umail.utah.edu>
2023-09-01 14:20:36 -0600

    Committing Day9 folder

--- Day9/BookAnalyzer/BookAnalyzer/main.cpp
+++ Day9/BookAnalyzer/BookAnalyzer/main.cpp
@@ -12,13 +12,17 @@
 #include <fstream>
 
 int main(int argc, const char * argv[]) {
-    
+    //Read in book file and assign variable name
     std::ifstream bookOne("testfile.txt");
+    //Assign varialbe
+    std::string singleWord;
+    std::vector<std::string> allWords;
+    while (bookOne >> singleWord){
+        allWords.push_back(singleWord);
+    }
+   
     
 
-    int i;
-    for(
-    }
     
     
     
